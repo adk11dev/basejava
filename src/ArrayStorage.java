cf75abdfacce854aecdfe8970f36ef9708ebdf27
Darkhan <adk11dev@gmail.com>
2022-05-19 13:36:20 +0600

    HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,50 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[size++] = resume;
     }
 
     Resume get(String uuid) {
+        int indexResume = getResumeIndexByUuid(uuid);
+
+        if (indexResume >= 0) return storage[indexResume];
+
         return null;
     }
 
     void delete(String uuid) {
+        int indexResume = getResumeIndexByUuid(uuid);
+
+        if (indexResume >= 0) System.arraycopy(storage, indexResume + 1, storage, indexResume, size - indexResume);
+        size--;
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     Resume[] getAll() {
-        return new Resume[0];
+        if (size < 1) return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
-        return 0;
+        return size;
+    }
+
+    private int getResumeIndexByUuid(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) return i;
+        }
+
+        return -1;
     }
 }
